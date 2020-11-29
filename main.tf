8aa2d56823c7656f82e5002b03984c6226f47357
Matthew Davis <matthew@matthewdavis.io>
2020-11-29 15:57:53 -0500

    bump

--- main.tf
+++ main.tf
@@ -5,7 +5,7 @@ resource "null_resource" "installer" {
 
     provisioner "local-exec" {
 
-        command = "KUBE_HOST=${ var.host } KUBE_TOKEN=${ var.token } kubectl apply -f https://github.com/kedacore/keda/releases/download/v2.0.0/keda-2.0.0.yaml"
+        command = "kubectl apply --server=${ var.host } --token=${ var.token } -f https://github.com/kedacore/keda/releases/download/v2.0.0/keda-2.0.0.yaml"
 
     }
 
